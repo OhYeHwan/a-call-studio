ed845bf59998a5e75f6b53c426282d4b0706a19d
OhYeHwan <azss0470@gmail.com>
2022-05-02 10:52:42 +0900

    더미 데이터 제거

--- src/store/ContentStore.js
+++ src/store/ContentStore.js
@@ -253,34 +253,9 @@ class ContentStore {
         contentList[item.superAppName] = [contentInfo];
       }
     });
-    contentList["MyApp"] = [
-      {
-        id: "1",
-        title: "Myapp_로또말줄임표나오나안나오나",
-        fileSize: "254MB",
-        type: "Myapp",
-      },
-      { id: "2", title: "Myapp_타로", fileSize: "254KB", type: "Myapp" },
-      { id: "3", title: "Myapp_노트", fileSize: "254KB", type: "Myapp" },
-      { id: "4", title: "Myapp_로또", fileSize: "254KB", type: "Myapp" },
-      { id: "5", title: "Myapp_타로", fileSize: "254KB", type: "Myapp" },
-      { id: "6", title: "Myapp_노트", fileSize: "1GB", type: "Myapp" },
-      { id: "7", title: "Myapp_로또", fileSize: "254KB", type: "Myapp" },
-      { id: "8", title: "Myapp_타로", fileSize: "254MB", type: "Myapp" },
-      { id: "9", title: "Myapp_노트", fileSize: "1GB", type: "Myapp" },
-      { id: "10", title: "Myapp_로또", fileSize: "1GB", type: "Myapp" },
-      { id: "11", title: "Myapp_타로", fileSize: "1GB", type: "Myapp" },
-      { id: "12", title: "Myapp_노트", fileSize: "1GB", type: "Myapp" },
-      { id: "13", title: "Myapp_로또", fileSize: "1GB", type: "Myapp" },
-      { id: "14", title: "Myapp_타로", fileSize: "1GB", type: "Myapp" },
-      { id: "15", title: "Myapp_노트", fileSize: "1GB", type: "Myapp" },
-    ];
+    contentList["MyApp"] = [];
 
-    contentList["Pay"] = [
-      { id: "16", title: "Pay_가디건", fileSize: "10MB", type: "Pay" },
-      { id: "17", title: "Pay_교재", fileSize: "10MB", type: "Pay" },
-      { id: "18", title: "Pay_가방", fileSize: "10MB", type: "Pay" },
-    ];
+    contentList["Pay"] = [];
 
     return contentList;
   }
